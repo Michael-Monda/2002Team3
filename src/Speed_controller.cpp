2d1f0363ba193da6f52f505b308f2c469f8a2368
Michael Monda <michaeljmonda@outlook.com>
2023-11-27 17:12:05 -0500

    harvested extant speed controller code

--- src/Speed_controller.cpp
+++ src/Speed_controller.cpp
@@ -1,13 +1,16 @@
 #include <Romi32U4.h>
 #include "Encoders.h"
 #include  "Speed_controller.h"
+#include "Position_estimation.h"
 
 Romi32U4Motors motors;
-Encoder MagneticEncoder; 
+Encoder MagneticEncoder;
+Position odometer; 
 
 void SpeedController::Init(void)
 {
     MagneticEncoder.Init();
+    odometer.Init();
 }
 
 void SpeedController::Run(float target_velocity_left, float target_velocity_right)
@@ -23,14 +26,54 @@ void SpeedController::Run(float target_velocity_left, float target_velocity_righ
         float u_right = Kp*e_right + Ki*E_right;
 
         motors.setEfforts(u_left,u_right);
+        odometer.UpdatePose(target_velocity_left,target_velocity_right);
         
-        Serial.print(MagneticEncoder.ReadVelocityLeft());
-        Serial.print('\t');
-        Serial.println(MagneticEncoder.ReadVelocityRight());
+        // Serial.print(MagneticEncoder.ReadVelocityLeft());
+        // Serial.print('\t');
+        // Serial.println(MagneticEncoder.ReadVelocityRight());
     }
+}
+boolean SpeedController::Turn(int degree, int direction)
+{
+    motors.setEfforts(0, 0);
+    int turns = counts*(degree/180.0); //assignment 1: convert degree into counts
+    int count_turn = MagneticEncoder.ReadEncoderCountLeft();
+
+    while(abs(abs(count_turn) - abs(MagneticEncoder.ReadEncoderCountLeft())) <= turns)
+    {
+        if(!direction) Run(50,-50);
+        else Run(-50,50);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
+}
+boolean SpeedController::Straight(int target_velocity, int time) //in mm/s and s
+{
+    motors.setEfforts(0, 0);
+    unsigned long now = millis();
+
+    while ((unsigned long)(millis() - now) <= time*1000){
+        Run(target_velocity,target_velocity);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
+}
+
+boolean SpeedController::Curved(int target_velocity_left, int target_velocity_right, int time) //in mm/s and s
+{
+    motors.setEfforts(0, 0);
+    
+    unsigned long now = millis();
+
+    while ((unsigned long)(millis() - now) <= time*1000){
+        Run(target_velocity_left,target_velocity_right);
+    }
+    motors.setEfforts(0, 0);
+    return 1;
 }
 
 void SpeedController::Stop()
 {
     motors.setEfforts(0,0);
+    odometer.Stop();
 }
