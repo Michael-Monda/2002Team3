d2c0273031e55e809f24cd970c881154fa5c4161
Michael Monda <michaeljmonda@outlook.com>
2023-12-10 11:16:59 -0500

    hh

--- src/behaviors.cpp
+++ src/behaviors.cpp
@@ -11,62 +11,64 @@
 #include "Sonar_sensor.h"
 
 //sensors
-IMU_sensor LSM6;
+IMU_sensor leyte;
 IRsensor ranger;
 SonarSensor hornet;
 
 Romi32U4ButtonA buttonA;
 
 // complext data-type median filter
-Algorithm med_x;
-Algorithm med_y;
-Algorithm med_z;
+Algorithm gyro_x_stab;
+Algorithm gyro_y_stab;
+Algorithm gyro_z_stab;
 
 //motor-speed controller
-SpeedController PIcontroller;
-WallFollowingController PDcontroller;
+SpeedController DriveControl;
+WallFollowingController FollowControl;
 Romi32U4Motors drivetrain;
 
 
 void Behaviors::Init(void)
 {
-    LSM6.Init();
-    med_x.Init();
-    med_y.Init();
-    med_z.Init();
-    PIcontroller.Init();
-    PDcontroller.Init();
+    leyte.Init();
+    ranger.Init();
+    hornet.Init();
+    gyro_x_stab.Init();
+    gyro_y_stab.Init();
+    gyro_z_stab.Init();
+    DriveControl.Init();
+    FollowControl.Init();
 }
 
 boolean Behaviors::DetectCollision(void)
 {
-    auto data_acc = LSM6.ReadAcceleration();
-    data[0] = med_x.ComplexTypeFilter(data_acc.X)*0.061;
-    data[1] = med_y.ComplexTypeFilter(data_acc.Y)*0.061;
-    data[2] = med_z.ComplexTypeFilter(data_acc.Z)*0.061;
+    auto data_acc = leyte.ReadAcceleration();
+    data[0] = gyro_x_stab.ComplexTypeFilter(data_acc.X)*0.061;
+    data[1] = gyro_y_stab.ComplexTypeFilter(data_acc.Y)*0.061;
+    data[2] = gyro_z_stab.ComplexTypeFilter(data_acc.Z)*0.061;
     if((data[0]) < threshold) return 1;
     else return 0;
 }
 
 boolean Behaviors::DetectBeingPickedUp(void)
 {
-    auto data_pickup = LSM6.ReadAcceleration();
-    data[0] = (med_x.ComplexTypeFilter(data_pickup.X) * 0.061);
-    data[1] = (med_y.ComplexTypeFilter(data_pickup.Y) * 0.061);
-    data[2] = (med_z.ComplexTypeFilter(data_pickup.Z) * 0.061);
+    auto data_pickup = leyte.ReadAcceleration();
+    data[0] = (gyro_x_stab.ComplexTypeFilter(data_pickup.X) * 0.061);
+    data[1] = (gyro_y_stab.ComplexTypeFilter(data_pickup.Y) * 0.061);
+    data[2] = (gyro_z_stab.ComplexTypeFilter(data_pickup.Z) * 0.061);
     if((abs(data[2])) > threshold_pickup) return 1;     // this returns only if the romi is being lifted.
     else return 0;
 }
 
 void Behaviors::Stop(void)
 {
-    PIcontroller.Stop();
+    DriveControl.Stop();
 }
 
 // IDLE, DRIVE, REVERSE, TURN
 void Behaviors::Run(void)
 {
-    auto data_crash = LSM6.ReadAcceleration();
+    auto data_crash = leyte.ReadAcceleration();
 
     switch (robot_state)
     {
@@ -74,10 +76,10 @@ void Behaviors::Run(void)
     {
         if(buttonA.getSingleDebouncedRelease()) {
             robot_state = DRIVE; 
-            PIcontroller.Stop(); //action
+            DriveControl.Stop(); //action
         } else { //transition condition
             robot_state = IDLE; 
-            PIcontroller.Stop(); //action 
+            DriveControl.Stop(); //action 
         }   
     break;
     }
@@ -86,16 +88,16 @@ void Behaviors::Run(void)
     {
         if (buttonA.getSingleDebouncedRelease() || DetectBeingPickedUp() == true) { //transition condition
             robot_state = IDLE;
-            PIcontroller.Stop();
+            DriveControl.Stop();
         }
 
         if (DetectCollision()) {
             robot_state = REVERSE;
-            PIcontroller.Stop();
+            DriveControl.Stop();
         }
 
         else {
-            PIcontroller.Run(speed, speed);
+            DriveControl.Run(speed, speed);
         }
 
     break;
@@ -103,7 +105,7 @@ void Behaviors::Run(void)
 
     case REVERSE:
     {   // back up for 10 centimeters
-        PIcontroller.Reverse(50, 10);
+        DriveControl.Reverse(50, 10);
         robot_state = TURN;
         
         //else if (backup condition == true) switch to turn state.
@@ -112,7 +114,7 @@ void Behaviors::Run(void)
 
     case TURN:
     {
-        PIcontroller.Turn(90, false);
+        DriveControl.Turn(90, false);
         robot_state = DRIVE;
 
     break;
